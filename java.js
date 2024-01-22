1b1e268dc4b9649b2808dc8bafc1dc509262c372
danielcad01@gmail.com <danielcad01@brightskills.com>
2024-01-22 16:48:44 +0530

    all file added

---
+++ java.js
@@ -0,0 +1,167 @@
+/*
+console.log("welcome to java");
+console.log(10);
+console.log(10.10);
+console.log({fname:"santhos",gender:"male"})
+
+var a=10;
+console.log(a);
+ var a=155;
+ var b="daniel";
+ var c="santhos";
+ var L=9854;
+ s={fname:"dani" ,fname:"santohs"};
+ console.log(b);
+ console.log(c);
+ console.log(a);
+ console.log(a);
+ console.log(s);
+  
+
+ function gradechecking(m){
+    if (m>=0 && m<=34)
+    {
+    console.log("conform");
+}
+else{
+     console.log("not conform");    
+}
+ }
+  
+gradechecking(38);
+
+console.clear();
+ // switch
+ function switchcase(m)
+ {
+    switch(m){
+        case 1:
+            console.log("dani");
+            break;
+            case 2:
+            console.log("santhos");
+            break;
+            case 3:
+            console.log("rahuman");
+            break;
+        default : console.log("noting data");
+    }
+ }
+switchcase(6);*/
+/*function GetResultFun(){
+
+var text="habibi come  to dubai";
+/*console.log(text.length);
+console.log(text.indexOf("come"));
+console.log(text.indexOf("dubai",10));
+console.log(text.lastIndexOf("come"));
+console.log(text.search("come"));*/
+/*let arr=text.search(/habibi/);
+console.log(arr);
+let arr1=text.match(/habibi/);
+console.log(arr1);
+let arr11=text.match(/habibi/);
+console.log(arr11);
+console.log(text.slice(1));
+
+
+//console.clear();*/
+
+
+// date usage
+
+/*let today= new Date();
+
+console.log(today);
+
+console.log(today.toDateString());
+console.log(today.toLocaleDateString());
+console.log(today.toISOString());
+console.log(today.toLocaleString());
+console.log(today.toString());
+console.log(today.toTimeString());
+console.log(today.toUTCString());
+console.log(today.getFullYear());
+console.log(today.getDate());
+console.log(today.getHours());
+console.log(today.getMilliseconds());
+console.log(today.getSeconds());
+
+console.log(today.getMonth());
+
+var month=["jan", "feb", "mar", "apr" ];
+console.log(month[today.getMonth()]);
+
+//let dob=new Date("2004-12-05");
+//console.log(dob);
+
+
+//let age =today.getFullYear()-dob.getFullYear();
+console.log(age);
+
+
+}
+GetResultFun();*/
+function GetResultFun(){
+
+    var text= "     welcome to welcome javascript to program organized by brightskills and brightskills welcomes to you once again    ";
+    
+   // var output=text.toUpperCase();
+   // console.log(output);
+    //var output=text.toLowerCase();
+    //console.log(output);
+    
+   var output=text.slice(0,);
+    console.log(output);
+    
+    //var output=text.slice(-10);
+    //console.log(output);
+    
+    
+    //var output=text.substring(0,4);
+    //console.log(output);
+    //var output=text.substring(-10);
+    //console.log(output);
+    
+    
+    
+    //var output=text.charAt(0);
+   // console.log(output);
+    
+    //var output=text.charCodeAt(0);
+    //console.log(output);
+    
+    //var output=text.startsWith("w");
+    //console.log(output);
+    
+    
+    //var output=text.endsWith("n");
+    //console.log(output);
+    
+    
+    //var output=text.replace('welcome', 'hi');
+    //console.log(output);
+    
+    //var output=text.replaceAll('welcome', 'hi');
+    //console.log(output);
+    
+    //var number="5";
+    //number=number.padStart(5,"B");
+    //console.log(number);
+    
+    //var number="5";
+    //number=number.padEnd(5,"B");
+    //console.log(number);
+    
+    
+    //split
+    
+   // console.table(text.split(" "));
+    
+    //console.log(text.length);
+    //var text1=text.trim();
+    //console.log(text1.length);
+    
+    }
+    
+    GetResultFun();
